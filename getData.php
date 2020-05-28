5ed530943d5492758262e2df20b4bc496738e006
YIDE0531 <54140752+YIDE0531@users.noreply.github.com>
2020-05-28 12:01:12 +0800

    Update getData.php

--- getData.php
+++ getData.php
@@ -1,4 +1,11 @@
 <?php
-echo "123";
+    $output= callpy();
+    $jsondata = shell_exec($output);
+    $obj = json_decode($jsondata);
+    echo base64_decode($jsondata);
 
+    function callpy() {
+        $jsondata= escapeshellcmd("py D:/FoodPanda/Python/main.py");
+        return $jsondata ;
+    }
 ?>
